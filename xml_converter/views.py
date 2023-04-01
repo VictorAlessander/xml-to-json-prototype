407aded86c6f07acfdfc0f2f85d24da73fad70f7
Victor Alessander <alessander@protonmail.com>
2023-03-31 23:20:00 -0300

    fix: format to be returned when the xml is converted to json

--- xml_converter/views.py
+++ xml_converter/views.py
@@ -6,7 +6,6 @@ from .helpers import XMLConverter
 
 
 def upload_page(request):
-    json_result = None
     error = None
 
     if request.method == "POST":
@@ -15,12 +14,10 @@ def upload_page(request):
         if form.is_valid():
             try:
                 xml_converter = XMLConverter(request.FILES["file"].read())
-                json_result = xml_converter.to_json()
+                return JsonResponse(xml_converter.to_json())
             except Exception as exc:
                 # Something to log the error (not like this in a real code)
                 print(exc)
                 error = "Something went wrong. Ensure the file is valid and try again."
 
-    return render(
-        request, "upload_page.html", dict(data=json_result, error=error)
-    )
+    return render(request, "upload_page.html", dict(error=error))
